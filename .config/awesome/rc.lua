cc350ec63bfabb57a21426e9787fa81678ec023e
Jon Nordby <jononor@gmail.com>
2017-03-13 20:41:36 +0100

    awesome: Fix commands for setting music volume

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -244,8 +244,14 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
+    -- Music controls
+    awful.key({ "Control", "Mod1" }, "/", function () awful.util.spawn("amixer sset Master toggle") end),
+    awful.key({ "Control", "Mod1" }, ".", function () awful.util.spawn("amixer sset Master 5%+") end),
+    awful.key({ "Control", "Mod1" }, ",", function () awful.util.spawn("amixer sset Master 5%-") end),
+
     -- Prompt
     awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end)
+
 )
 
 clientkeys = awful.util.table.join(
