ed630d9084d463dc394db0ed944b579dbdc2451d
Jon Nordby <jononor@gmail.com>
2022-02-13 20:57:24 +0100

    awesome: Unhardcode theme path

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -37,7 +37,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "gnome-terminal"
