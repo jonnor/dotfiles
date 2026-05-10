b0b3fec74280f3c3924674161d90fa06955f47ac
Jon Nordby <jononor@gmail.com>
2026-05-10 11:51:21 +0200

    devtools: Setup connection to llama-cpp

--- .jonnor/devtools/emlearn-micropython/container.sh
+++ .jonnor/devtools/emlearn-micropython/container.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-DOCKER_TAG=emlearn-micropython-dev
+DOCKER_TAG=emlearn-micropython-dev:latest
 WORKSPACE_DIR=/home/jon/projects/emlearn-micropython
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -12,13 +12,25 @@ if [[ -z "${OPENROUTER_API_KEY}" ]]; then
   exit 1
 fi
 
+if [[ -z "${LLAMA_API_KEY_JON_WORKSTATION}" ]]; then
+  echo "Warning: LLAMA_API_KEY_JON_WORKSTATION is not set" >&2
+  #exit 1
+fi
+
 # rebuild docker image (if needed)
 cd $DIR
 cp $WORKSPACE_DIR/requirements*.txt ./
 podman build . -t $DOCKER_TAG
 
+# XXX: no-aaaa was critical to avoid AAAA IPv6 lookups on mDNS failing
+# and causing huge lags
+ 
 # run the container
 exec podman run -it --userns=keep-id \
+    --log-level=debug \
+    --network=bridge \
+    --dns-opt single-request-reopen --dns-opt no-aaaa \
     -v $WORKSPACE_DIR:/workspace \
     -e OPENROUTER_API_KEY \
+    -e LLAMA_API_KEY_JON_WORKSTATION \
     $DOCKER_TAG
--- .jonnor/devtools/emlearn-micropython/entrypoint.sh
+++ .jonnor/devtools/emlearn-micropython/entrypoint.sh
@@ -2,4 +2,18 @@
 
 cp /config/tmux.conf "$HOME/.tmux.conf"
 
+mkdir -p "$HOME/.pi/agent"
+cp /config/pi/agent/* "$HOME/.pi/agent"
+
+# use local DNS only server for lookup for mDNS etc
+# podman via pasta does the proxying
+# NOTE: requires systemd-resolved or similar on the host
+# fails because not root
+#cat > /etc/resolv.conf << 'EOF'
+#nameserver 169.254.1.1
+#options single-request-reopen
+#EOF
+
+echo "resolv.conf overridden"
+
 exec "$@"
